351d25cde8c4a042d6a346673cf0565f5c1c058f
fpiuzzi <147161762+fpiuzzi@users.noreply.github.com>
2024-12-11 21:49:11 +0100

    Create Invoice Management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
+import { FactureComponent } from './facture/facture.component';
+import { Factures2Component } from './factures2/factures2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'factures', component: FactureComponent },
+  { path: 'factures2', component: Factures2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ContactComponent } from './contact/contact.component';
+import { FactureComponent } from './facture/facture.component';
+import { Factures2Component } from './factures2/factures2.component';
+import { AddFactureComponent } from './facture/add-facture/add-facture.component';
+import { ListFactureComponent } from './facture/list-facture/list-facture.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ContactComponent,
+    FactureComponent,
+    Factures2Component,
+    AddFactureComponent,
+    ListFactureComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
